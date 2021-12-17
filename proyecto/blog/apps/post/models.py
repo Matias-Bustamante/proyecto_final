e40f0e3ba38e8a6415ccb0a17c20e622ec00fa5c
Matias-Bustamante <matybustamante151@gmail.com>
2021-12-16 21:27:15 -0300

    estructura base

---
+++ proyecto/blog/apps/post/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model): 
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField("Nombre de la Categoria" , max_length=150)
+    descripcion=models.CharField('Descripción de la Categoria' ,max_length=150)
+    estado=models.BooleanField('Activado/Desactivado', default=True)
+    
+
+
+
+    class Meta(): 
+        verbose_name="Categoria"
+        verbose_name_plural="Categorias"
+    
+    def __str__(self): 
+        return self.nombre
+
+  
+
+
+class Post(models.Model): 
+    id=models.AutoField(primary_key=True); 
+    titulo=models.CharField('Titulo del Post', max_length=150, null=False, blank=False)
+    descripcion=models.CharField('Descripción', max_length=110, null=False, blank=False)
+    categoria=models.ForeignKey(Categoria,on_delete=models.CASCADE,null=False,blank=False)
+    fecha_alta=models.DateField('Fecha de creacion',auto_now_add=True, auto_now=False)
+    estado=models.BooleanField('Publicado/No Publicado', default=True)
+    imagen=models.URLField(max_length=600, blank=False, null=False)
+    contenido=models.TextField()
+    fecha_creacion=models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+
+
+    class Meta(): 
+        verbose_name="Post", 
+        verbose_name_plural="Posts"
+    
+    def __str__(self): 
+        return self.titulo
+
+
+class Comentario(models.Model): 
+    id=models.AutoField(primary_key=True); 
+    descripcion=models.TextField(); 
+    fecha_alta=models.DateField(auto_now_add=True, auto_now=False); 
+
+    class Meta(): 
+        verbose_name="Comentario", 
+        verbose_name_plural="Comentarios"
+
+    def __str__(self): 
+        return self.descripcion 
+
---
+++ proyecto/blog/blog/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'UTN', 
+        'USER':'postgres', 
+        'PASSWORD':'root', 
+        'HOST':'localhost', 
+        'PORT':'',
+    },
+}
+
